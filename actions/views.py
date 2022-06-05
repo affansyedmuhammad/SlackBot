eb3e7a1feb99ef2c4ede0763242529eb19c8bde0
affansyedmuhammad <affansyedmuhammad@gmail.com>
2022-06-06 01:01:57 +0500

    requirement.txt added

--- actions/views.py
+++ actions/views.py
@@ -10,29 +10,8 @@ from django.http import HttpResponse, JsonResponse
 import slack
 from .models import userInformation, messageInformation, upload
 import urllib.request 
-import re
 
 # Create your views here.
-
-def download(url: str, dest_folder: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)  # create folder if it does not exist
-
-    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
-    file_path = os.path.join(dest_folder, filename)
-
-    r = requests.get(url, stream=True)
-    if r.ok:
-        print("saving to", os.path.abspath(file_path))
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
-    else:  # HTTP status code 4XX/5XX
-        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
-
 @csrf_exempt
 def events(request):#function that recieves events from Slack API
 
