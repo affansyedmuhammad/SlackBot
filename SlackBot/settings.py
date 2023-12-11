d827b576283b1701827d72ae06bbcb5c288d1a87
affansyedmuhammad <92621623+affansyedmuhammad@users.noreply.github.com>
2023-12-12 01:50:27 +0500

    Update

--- SlackBot/settings.py
+++ SlackBot/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q^=+!qmezps_i-_+_!s7zvh!69z8dre61lb^7_ljrnl77lp0m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = TRUE
 
